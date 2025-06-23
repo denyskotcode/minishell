04affa2ffd98fce3dd76e91cca5b4bf21515060a
denyskotcode <kotdenis2004@gmail.com>
2025-06-23 13:14:00 +0200

    chore: initialize project structure with Makefile and global header
    Set up build system targeting macOS/Linux with readline support.
    Define all core data structures: t_token, t_group, t_exec_ctx,
    t_pipe_data, t_info, and forward-declare all module prototypes.
    Include libft as a static library dependency.

---
+++ Makefile
@@ -0,0 +1,48 @@
+# Vars
+CC = cc
+CFLAGS = -I$(SRCDIR) -I$(LIBFTDIR) -I/opt/homebrew/opt/readline/include -Wall -Wextra -Werror 
+NAME = minishell
+SRCDIR = src
+OBJDIR = obj
+LIBFTDIR = libs/libft
+
+SRC_BUILTIN = builtin/builtin.c builtin/utils.c builtin/cd.c builtin/export.c builtin/echo.c
+SRC_ENV = env/env.c env/utils.c env/init.c
+SRC_EXEC = exec/exec.c exec/external.c exec/path.c exec/redir.c exec/redir_utils.c exec/utils.c exec/heredoc.c exec/exec_utils.c exec/command_validation.c exec/exec_helpers.c exec/heredoc_utils.c exec/redir_helpers.c
+SRC_INIT = init/main.c init/reader.c init/init_shell_env.c
+SRC_LEXER = lexer/lexer.c lexer/handle.c lexer/handle_utils.c lexer/utils.c lexer/syntax_validator.c lexer/syntax_validator_utils.c
+SRC_PARSER = parser/parser.c parser/utils.c parser/expand.c parser/expand_utils.c parser/handlers.c parser/checker.c parser/groups.c
+SRC_PIPE = pipe/pipe.c pipe/child.c pipe/exec.c pipe/external.c pipe/setup.c
+SRC_UTILS = utils/utils.c utils/str.c
+SRC = $(SRC_BUILTIN) $(SRC_ENV) $(SRC_EXEC) $(SRC_INIT) $(SRC_LEXER) $(SRC_PARSER) $(SRC_PIPE) $(SRC_UTILS)
+
+OBJS = $(addprefix $(OBJDIR)/, $(SRC:.c=.o))
+SRCS = $(addprefix $(SRCDIR)/, $(SRC))
+
+# Rules of files
+$(NAME): $(OBJS) $(LIBFTDIR)/libft.a
+	$(CC) $(CFLAGS) $(OBJS) -L$(LIBFTDIR) -lft -L/opt/homebrew/opt/readline/lib -lreadline -o $(NAME)
+
+$(OBJDIR):
+	mkdir -p $(OBJDIR)/builtin $(OBJDIR)/env $(OBJDIR)/exec $(OBJDIR)/init $(OBJDIR)/lexer $(OBJDIR)/parser $(OBJDIR)/pipe $(OBJDIR)/utils
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c | $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFTDIR)/libft.a:
+	$(MAKE) -C $(LIBFTDIR)
+
+# Rules of actions
+.PHONY: all clean fclean re
+
+all: $(NAME)
+
+clean:
+	rm -rf $(OBJDIR)
+	$(MAKE) -C $(LIBFTDIR) clean
+
+fclean: clean
+	rm -rf $(NAME)
+	$(MAKE) -C $(LIBFTDIR) fclean
+
+re: fclean all
---
+++ src/init/header.h
@@ -0,0 +1,226 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 13:14:05 by dkot              #+#    #+#             */
+/*   Updated: 2025/07/18 19:25:07 by dkot             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <limits.h>
+
+# include "../../libs/libft/libft.h"
+
+typedef enum e_type
+{
+	WORD,
+	FIELD,
+	EXP_FIELD,
+	SEP,
+	PIPE,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	REDIRECT_APPEND,
+	HEREDOC
+}					t_e_type;
+
+typedef struct s_token
+{
+	t_e_type		type;
+	char			*value;
+	int				value_len;
+}					t_token;
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct s_info
+{
+	int				exit_f;
+	int				last_exit_status;
+	char			**env;
+}					t_info;
+
+typedef struct s_command
+{
+	char			*arg;
+}					t_command;
+
+typedef struct s_heredoc_data
+{
+	char			*delimiter;
+	int				fd;
+}					t_heredoc_data;
+
+typedef struct s_group
+{
+	t_list			*argv;
+	t_list			*heredocs;
+}					t_group;
+
+typedef struct s_exec_ctx
+{
+	int				stdin_backup;
+	int				stdout_backup;
+	int				last_exit_status;
+	char			**envp;
+	t_info			*info;
+}					t_exec_ctx;
+
+typedef int			(*t_builtin_func)(char **args, t_exec_ctx *ctx);
+
+typedef struct s_builtin
+{
+	char			*name;
+	t_builtin_func	func;
+}					t_builtin;
+
+typedef struct s_pipe_data
+{
+	int				pipe_count;
+	int				(*pipes)[2];
+	pid_t			*pids;
+	int				cmd_index;
+	t_list			*tmp;
+}					t_pipe_data;
+
+int			handle_whitespace(char **ptr, t_token *token);
+int			handle_pipe(char **ptr, t_token *token);
+int			handle_redirect_in(char **ptr, t_token *token);
+int			handle_redirect_out(char **ptr, t_token *token);
+int			is_valid_identifier(const char *str);
+int			is_valid_number(char *str);
+int			is_builtin(const char *cmd);
+int			execute_builtin(char **args, t_exec_ctx *ctx);
+int			builtin_cd(char **args, t_exec_ctx *ctx);
+int			builtin_echo(char **args, t_exec_ctx *ctx);
+int			builtin_pwd(char **args, t_exec_ctx *ctx);
+int			builtin_export(char **args, t_exec_ctx *ctx);
+int			builtin_unset(char **args, t_exec_ctx *ctx);
+int			builtin_env(char **args, t_exec_ctx *ctx);
+int			builtin_exit(char **args, t_exec_ctx *ctx);
+void		cleanup_tokens(t_list **tokens);
+void		free_token(t_token *token);
+int			process_token(char **ptr, t_token *token);
+int			execute_commands(t_list *groups, t_info *info);
+int			execute_single_command(t_group *grp, t_exec_ctx *ctx);
+int			count_groups(t_list *groups);
+void		init_exec_ctx(t_exec_ctx *ctx, t_info *info);
+void		update_exit_status(t_exec_ctx *ctx, t_info *info);
+void		cleanup_and_restore(t_exec_ctx *ctx, t_info *info);
+int			handle_heredoc_processing(t_list *groups, t_exec_ctx *ctx);
+int			execute_external(char **args, t_exec_ctx *ctx);
+int			execute_pipeline(t_list *groups, t_exec_ctx *ctx);
+char		**group_to_argv(t_group *grp);
+void		free_argv(char **argv);
+char		*resolve_command_path(const char *cmd, char **env);
+int			setup_redirections(char ***args, t_group *grp);
+int			handle_input_redirect_case(char **argv, int *i, int *last_error);
+int			handle_output_redirect_case(char **argv, int *i);
+int			handle_append_redirect_case(char **argv, int *i, int *last_error);
+int			find_and_use_heredoc(const char *delimiter, t_group *grp);
+int			handle_input_redirect(const char *filename);
+int			handle_output_redirect(const char *filename);
+int			handle_append_redirect(const char *filename);
+int			handle_heredoc(const char *delimiter);
+int			preprocess_heredocs(t_list *groups);
+int			create_heredoc_fd(const char *delimiter);
+int			handle_heredoc_redirect(int heredoc_fd);
+void		heredoc_child_process(int write_fd, const char *delimiter);
+int			count_args(t_list *args);
+void		cleanup(int stdin_temp, int stdout_temp, char **argv);
+void		remove_argv_element(char **argv, int index);
+int			create_pipes(t_pipe_data *data);
+void		close_all_pipes(int (*pipes)[2], int count);
+void		setup_child_pipes(t_pipe_data *data);
+int			wait_for_children(t_pipe_data *data);
+int			execute_pipe_commands(t_list *groups, t_exec_ctx *ctx,
+				t_pipe_data *data);
+void		execute_child_process(char **argv, t_exec_ctx *ctx,
+				t_pipe_data *data, t_group *grp);
+void		execute_external_child(char **argv, t_exec_ctx *ctx,
+				t_pipe_data *data);
+int			quotes_check(char *input);
+t_list		*lexer(char *input);
+t_list		*parser(t_list *tokens, char **env);
+t_list		*tokens_to_groups(t_list *tok_lst, char **env);
+t_command	*new_command(const char *arg);
+t_group		*new_group(void);
+int			add_argument(t_group *grp, const char *arg);
+void		free_groups(t_list **groups);
+char		*join_strings(char *s1, char *s2);
+int			find_env_var(char **env, const char *name);
+char		*expand_env_var(const char *var_name, char **env);
+char		*get_var_name(const char *str, int *len);
+char		*expand_word_env_process_var(const char **p, char **result,
+				char **env);
+char		*expand_word_env_process_char(const char **p, char **result,
+				char c);
+char		*expand_word_env(const char *src, char **env);
+char		*concatenate_text_tokens(t_list **scan, char **env);
+int			handle_redirect_create_group(t_group **cur, t_list **groups);
+int			handle_redirect(t_token *tk, t_list *tok_lst, t_group **cur,
+				t_list **groups);
+int			handle_text_create_group(t_group **cur, t_list **groups);
+int			handle_text(t_list **tok_lst, t_group **cur, t_list **groups,
+				char **env);
+int			is_redirect(t_e_type t);
+int			is_text(t_e_type t);
+int			is_valid_var_char(char c, int first);
+int			ft_init(t_info *info);
+void		handle_sigint(int sig);
+char		*ft_readline(const char *prompt);
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_free_tokens(t_list **tokens);
+char		*ft_strndup(const char *s, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
+void		write_error(const char *msg);
+void		write_error_with_arg(const char *prefix, const char *arg,
+				const char *suffix);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_strcpy(char *dst, const char *src);
+char		*ft_strcat(char *s1, const char *s2);
+char		**init_env_copy(char **envp);
+void		free_env_copy(char **env);
+char		**set_env_var(char **env, const char *name, const char *value,
+				int overwrite);
+char		**unset_env_var(char **env, const char *name);
+char		*get_env_value(char **env, const char *name);
+void		init_shell_env(t_info *info);
+int			is_command_token(t_e_type type);
+int			validate_pipe_syntax(t_list *tokens);
+int			validate_pipe_syntax_enhanced(t_list *tokens);
+t_list		*tokenize_input(char *input);
+int			process_token_for_validation(t_token *token,
+				int *has_command_before, int *expecting_command);
+int			process_token_for_enhanced_validation(t_token *token,
+				int	*has_command_before, int *expecting_command,
+				int *consecutive_pipes);
+int			validate_pipe_syntax_enhanced(t_list *tokens);
+int			validate_command(char *cmd_path, char *cmd_name);
+void		cleanup_memory(char **args, char **env);
+
+#endif
